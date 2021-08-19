ebb6e972a6852963aaef3dfc63e6621d055c7b17
imsat16 <wepeg44@gmail.com>
2021-08-19 17:19:37 +0700

    Create Project
    This is Basic Project

---
+++ mata/android/app/src/main/kotlin/com/example/mata/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mata
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mata/lib/export.dart
@@ -0,0 +1,3 @@
+library exsomething;
+
+export 'HomePage.dart';
